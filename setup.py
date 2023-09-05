b317c83290afd31ea481b3ed5bc58cd97cb2ae75
Enes Arslan <arslanenes515@gmail.com>
2023-09-05 19:50:53 +0300

    init: version0.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="arslan",
+    version="0.0.1",
+    author="Enes Arslan",
+    author_email="arslanenes515@gmail.com",
+    description="A simple Python helper package for commont tasks in projects and competitions.",
+    packages=["arslan"],
+    install_requires=["seaborn"],
+)
---
+++ visualizations.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from typing import List
+
+
+def barchart(x, y):
+
+    sns.barplot(x=x, y=y)
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    pass
